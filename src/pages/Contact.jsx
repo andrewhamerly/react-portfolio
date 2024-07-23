2d729e47fc68546ed193f27cba6b0352ed03214a
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-07-23 15:40:10 -0400

    Updated Resume and Contact pages.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -27,24 +27,44 @@ const Contact = () => {
     };
 
     return (
-        <div className="contact">
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" name="name" value={formState.name} onChange={handleChange} />
-                    {errors.name && <span className="error">{errors.name}</span>}
-                </label>
-                <label>
-                    Email:
-                    <input type="email" name="email" value={formState.email} onChange={handleChange} />
-                    {errors.email && <span className="error">{errors.email}</span>}
-                </label>
-                <label>
-                    Message:
-                    <textarea name="message" value={formState.message} onChange={handleChange}></textarea>
-                    {errors.message && <span className="error">{errors.message}</span>}
-                </label>
-                <button type="submit">Submit</button>
+        <div className="contact container my-4">
+            <form onSubmit={handleSubmit} className="contact-form">
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name:</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        className="form-control"
+                        value={formState.name}
+                        onChange={handleChange}
+                    />
+                    {errors.name && <span className="error text-danger">{errors.name}</span>}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="form-control"
+                        value={formState.email}
+                        onChange={handleChange}
+                    />
+                    {errors.email && <span className="error text-danger">{errors.email}</span>}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="message" className="form-label">Message:</label>
+                    <textarea
+                        name="message"
+                        id="message"
+                        className="form-control"
+                        value={formState.message}
+                        onChange={handleChange}
+                    ></textarea>
+                    {errors.message && <span className="error text-danger">{errors.message}</span>}
+                </div>
+                <button type="submit" className="btn btn-contact">Submit</button>
             </form>
         </div>
     );
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -33,7 +33,7 @@ const Resume = () => {
         <div className="container my-4">
             <div className="button-section text-center mb-4">
                 <a className="resumebtn btn m-2" href="https://andrewhamerly.com" target="_blank" rel="noopener noreferrer">View Website</a>
-                <a className="btn btn-secondary m-2" href="src/assets/Andrew-Hamerly-Resume-2024.pdf" download>Download Resume</a>
+                <a className="btn downloadbtn btn-secondary m-2" href="src/assets/Andrew-Hamerly-Resume-2024.pdf" download>Download Resume</a>
             </div>
         </div>
             <div className="row">
@@ -60,6 +60,35 @@ const Resume = () => {
                         <p>October 2018 - Present</p>
                         <p><span className="employment-italic">Self-Employed, Cleveland, OH</span></p>
                         <p>Created over 15 client websites for small business, retail, and E-commerce brands. Currently, maintain and update 10 client WordPress websites with security and performance best practices.</p>
+                    </div>
+                    <div className="experience">
+                        <h4>Website Developer</h4>
+                        <p>February 2023 - Present</p>
+                        <p><span className="employment-italic">Ascension Biomedical, Oberlin, OH</span></p>
+                        <p>Developed and currently maintain an Ohio Medical Marijuana Control Program compliant website that passed meticulous review by the State of Ohio Division of Cannabis Control.
+                        </p>
+                    </div>
+                    <div className="experience">
+                        <h4>Digital Marketing Manager</h4>
+                        <p>October 2019 - January 2023</p>
+                        <p><span className="employment-italic">Organic Plus Brands, Cleveland, OH</span></p>
+                        <p>Developed and implemented a modern digital marketing strategy to drive eCommerce sales 4x. Built the strategy, content, and websites for three in-house brands resulting in over 350k in online sales.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-9">
+                    <h2>EDUCATION</h2>
+                    <div className="education">
+                        <h4>Case Western Reserve University, Full Stack Web Development Boot Camp Certificate</h4>
+                        <p>February 2024 - August 2024</p>
+                        <p>An intensive program for gaining technical programming skills in HTML5, CSS3, JavaScript, jQuery, Bootstrap, Bulma, Tailwind,  PostgreSQL, Sequalize, MongoDB, Express.js, React.js, Node.js, and Handelbars.js.</p>
+                    </div>
+                    <div className="education">
+                        <h4>Baldwin Wallace University, Bachelor in Arts in Innovation & Entrepreneurship and Business Administration</h4>
+                        <p>Graduated 2017</p>
+                    </div>
+                    <div className="education">
+                        <h4>Cuyahoga Community College, Associate of Applied Business in Purchasing and Supply Chain Management</h4>
+                        <p>Graduated 2015</p>
                     </div>
                 </div>
             </div>
