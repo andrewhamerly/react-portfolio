bdf6506a7e5d61989c23e51fa90d3062b1974311
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-07-25 00:24:41 -0400

    Fixed issue with logo.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import './Header.css';
 import NavTabs from '../Navigation/Navigation.jsx';
+import logo from '../../assets/images/Ah-no-bkg.png';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="container">
-                    <img src="src/assets/images/Ah-no-bkg.png" alt="Andrew Hamerly" className="logo" />
+                    <img src={logo} alt="Andrew Hamerly Logo" className="logo" />
                 <nav>
                     <NavTabs />
                 </nav>
